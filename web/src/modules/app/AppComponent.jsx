0612b309d39619001d9c5b607c228105b448943e
yehua <1204030083@qq.com>
2017-11-14 14:51:31 +0800

    页面样式修改

--- web/src/modules/app/AppComponent.jsx
+++ web/src/modules/app/AppComponent.jsx
@@ -77,32 +77,52 @@ class AppComponent extends Component {
         //socket.emit('message',{content:this.refs.name.value})
         this.setCookie('currentUser', this.refs.name.value);
         this.setState({currenName:this.getCookie('currentUser')})
+        this.refs.logBtn.disabled = true;
+        this.refs.quitBtn.disabled = false;
         console.log(this.state.currenName)
     }
     quit(){
+        this.refs.logBtn.disabled = false;
+        this.refs.quitBtn.disabled = true;
         socket.emit('disconnect', function () {
             // console.log('you have been disconnected');
         });
     }
     sendMsg(){
         socket.emit('message',{userName:this.state.currenName, content:this.refs.msg.value})
+        this.refs.msg.value = '';
+        this.refs.msg.focus();
+    }
+    enterMsg(e){
+        var char = e.which || e.keyCode;
+        if(char == 13){
+            this.sendMsg();
+        }
+        e.preventDefault()
+    }
+    enterName(e){
+        var char = e.which || e.keyCode;
+        if(char == 13){
+            this.sendName();
+        }
+        e.preventDefault()
     }
     render() {
         return (
         	   <div className="appmain">
-                    {this.state.nameText ? <Popbox text={this.state.nameText}/> : ''}
+                    {this.state.nameText != this.state.currenName ? <Popbox text={this.state.nameText}/> : ''}
                     <div style={{overflow:'hidden'}}><h1>Together chat</h1> <span>在线人数：{this.state.onlineCount}</span>
                         <span style={{float:'right'}}>当前登录为：{this.state.currenName}</span>
                     </div>
                     <ul className="showMsg" ref="showMsg"></ul>
                     
                     <div className="inputBox">
-                        <input type="text" className="form-control input-name"  placeholder="请输入你的昵称！" ref="name"/>
-                        <button type="submit" className="btn btn-default" onClick={this.sendName.bind(this)}>登录</button>
-                        <button type="submit" className="btn btn-default" onClick={this.quit.bind(this)}>退出</button>
+                        <input type="text" className="form-control input-name"  placeholder="请输入你的昵称！" ref="name" onKeyDown={this.enterName.bind(this)}/>
+                        <button type="submit" className="btn btn-default log-btn" ref="logBtn" onClick={this.sendName.bind(this)}>登录</button>
+                        <button type="submit" className="btn btn-default" ref="quitBtn" onClick={this.quit.bind(this)} disabled>退出</button>
                     </div>
-                    <textarea className="form-control" rows="3" ref="msg"></textarea>
-                    <button type="submit" className="btn btn-default" onClick={this.sendMsg.bind(this)}>send</button>
+                    <textarea className="form-control" rows="3" ref="msg" onKeyDown={this.enterMsg.bind(this)}></textarea>
+                    <button type="submit" className="btn btn-success" onClick={this.sendMsg.bind(this)}>send</button>
                     {this.props.children}
                </div>  		
         )
